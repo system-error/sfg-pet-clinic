38e714c6e314435dff775235b1964ad6a6c1f5ef
system-error <christos_dousmanopoulos@yahoo.gr>
2021-02-04 10:32:39 +0200

    adding POJOs. Closes #2

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class PetType {
+    private String Name;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}

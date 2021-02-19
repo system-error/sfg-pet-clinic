de934f50d5d2991160181fb5aff38a47dfefd09c
system-error <christos_dousmanopoulos@yahoo.gr>
2021-02-19 14:35:25 +0200

    Created Vet Speciality Entity Assosiate to Vet. closes #28

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,4 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+
+    private Set<Specialty> specialty;
+
+    public Set<Specialty> getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(Set<Specialty> specialty) {
+        this.specialty = specialty;
+    }
 }

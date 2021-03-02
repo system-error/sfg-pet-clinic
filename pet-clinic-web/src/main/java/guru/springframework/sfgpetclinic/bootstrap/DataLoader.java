1b01826b60b042ea10ae21df45f24bc8cdc16fb8
system-error <christos_dousmanopoulos@yahoo.gr>
2021-03-02 12:24:03 +0200

    Created Specialty Map Service impl. closes #32

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -18,6 +18,8 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetTypeService petTypeService;
+    private String test;
+
 
 
     public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
